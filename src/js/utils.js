aa07b609c58156f9d7a64cb33de917f376a0ea6b
arkadyKid <kidanov.a.y1986@gmail.com>
2020-08-20 16:24:49 +0300

    change rendering

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,19 +1,19 @@
 export const setDefaultSort = () => {
-  const priceUpButton = document.querySelector('button[data-sort="price-up"]');
-  priceUpButton.parentElement.classList.add('sort__item--active');
+  const priceUpButtonElement = document.querySelector('button[data-sort="price-up"]');
+  priceUpButtonElement.parentElement.classList.add('sort__item--active');
 };
 
 export const showResetButton = () => {
-  const resetButton = document.querySelector('.form__reset');
+  const resetButtonElement = document.querySelector('.form__reset');
 
   const onFormChange = () => {
-    resetButton.classList.remove('form__reset--hide');
+    resetButtonElement.classList.remove('form__reset--hide');
   };
   const onResetButtonClick = () => {
-    if (!resetButton.classList.contains('form__reset--hide')) {
-      resetButton.classList.add('form__reset--hide');
+    if (!resetButtonElement.classList.contains('form__reset--hide')) {
+      resetButtonElement.classList.add('form__reset--hide');
     }
   };
   document.forms[0].addEventListener('change', onFormChange);
-  resetButton.addEventListener('click', onResetButtonClick);
+  resetButtonElement.addEventListener('click', onResetButtonClick);
 };
