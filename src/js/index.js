3098ca1881e0be2d6ba45fa5626d3133596ca90e
arkadyKid <kidanov.a.y1986@gmail.com>
2020-08-20 14:34:18 +0300

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 import service from './service';
-import showResetButton from './show-reset-button';
+import { showResetButton, setDefaultSort } from './utils';
 import filter from './filter';
 import sort from './sort';
 import renderItems from './render-items';
 
 renderItems(service.getData());
 showResetButton();
+setDefaultSort();
 filter();
 sort();
